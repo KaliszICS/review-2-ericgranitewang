d0d5774f78f088f6d204c5a768ba804a3cc6c40f
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-02-03 14:48:37 +0000

    me when the commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,42 +1,52 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		Scanner in = new Scanner (System.in);
 		q1();
 		q2();
 		q3();
 		q4();
 		q5();
 		q6();
+		in.close();
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+		System.out.print("In: ");
+		System.out.println(in.nextLine());
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+		System.out.print("In: ");
+		int a = in.nextInt();
+		int b = in.nextInt();
+		in.nextLine();
+		System.out.println(a/b);
+		System.out.println(a%b);
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+		System.out.print("In: ");
+		System.out.println(in.nextLine() + " was the text you wrote");
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+		System.out.print("In: ");
+		System.out.println(in.nextInt() * 5));
+		in.nextLine();
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+		System.out.print("In: ");
+		System.out.println(in.nextBoolean());
+		in.nextLine();
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+		System.out.print("In: ");
+		System.out.println(in.nextDouble());
+		in.nextLine();
 	}
 
 }
