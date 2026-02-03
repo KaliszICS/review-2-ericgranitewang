2dc160449f93dbd17cd2c286e96c2ec4339c8f23
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-02-03 14:52:14 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,52 +1,61 @@
 import java.util.Scanner;
 public class PracticeProblem {
-
 	public static void main(String args[]) {
-		Scanner in = new Scanner (System.in);
 		q1();
 		q2();
 		q3();
 		q4();
 		q5();
 		q6();
-		in.close();
 	}
 
 	public static void q1() {
+		Scanner in = new Scanner (System.in);
 		System.out.print("In: ");
 		System.out.println(in.nextLine());
+		in.close();
 	}
 
 	public static void q2() {
+		Scanner in = new Scanner (System.in);
 		System.out.print("In: ");
 		int a = in.nextInt();
 		int b = in.nextInt();
 		in.nextLine();
 		System.out.println(a/b);
 		System.out.println(a%b);
+		in.close();
 	}
 
 	public static void q3() {
+		Scanner in = new Scanner (System.in);
 		System.out.print("In: ");
 		System.out.println(in.nextLine() + " was the text you wrote");
+		in.close();
 	}
 
 	public static void q4() {
+		Scanner in = new Scanner (System.in);
 		System.out.print("In: ");
 		System.out.println(in.nextInt() * 5));
 		in.nextLine();
+		in.close();
 	}
 
 	public static void q5() {
+		Scanner in = new Scanner (System.in);
 		System.out.print("In: ");
 		System.out.println(in.nextBoolean());
 		in.nextLine();
+		in.close();
 	}
 
 	public static void q6() {
+		Scanner in = new Scanner (System.in);
 		System.out.print("In: ");
 		System.out.println(in.nextDouble());
 		in.nextLine();
+		in.close();
 	}
 
 }
